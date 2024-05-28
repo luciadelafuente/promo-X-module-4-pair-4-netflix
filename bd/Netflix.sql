97f69844dc2d611633c3c216cb0927ddde1412fa
luciadelafuente <luciadelaf1991@gmail.com>
2024-05-28 10:53:17 +0200

    add sql file

---
+++ bd/Netflix.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE Netflix;
+USE Netflix;
+
+CREATE TABLE movies(
+idMovies INT auto_increment primary key,
+title VARCHAR (45) not null,
+genre VARCHAR (45) not null,
+image VARCHAR (1000) not null,
+category VARCHAR (45) not null,
+year SMALLINT
+);
+
+CREATE TABLE Users(
+idUser INT auto_increment primary key,
+user VARCHAR (45) not null,
+password VARCHAR (45) not null,
+name VARCHAR (45) not null,
+email VARCHAR (45) not null,
+plan_details VARCHAR (45) not null
+);
+
+CREATE TABLE Actors(
+idActor INT auto_increment primary key,
+name VARCHAR (45) not null,
+lastname VARCHAR (45) not null,
+country VARCHAR (45) not null,
+birthday DATE
+);
